7d8f818f20715f2ac12f12e1d9eba17f67363edb
sebmiet <sebmiet@hotmail.com>
2021-02-28 00:11:43 +0100

    css and begin to make todo component

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -1,35 +1,38 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const CreateTodo = (props) => {
+  const [input, setInput] = useState("");
+  const [todoId, setTodoId] = useState(0);
 
-const [input, setInput] = useState("");
-const [todoId, setTodoId] = useState(0);
-
-const handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     setInput(e.target.value);
-}
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     props.addTodo({
-        id: todoId,
-        text: input,
-        createTime: new Date(),
+      id: todoId,
+      text: input,
+      isCompleted: false,
+      createTime: new Date(),
+      wasEdited: false,
+      lastEdit: null,
     });
-    setTodoId(todoId + 1);   
-    setInput('');
-}
-
-
+    setTodoId(todoId + 1);
+    setInput("");
+  };
 
   return (
-    
-      <form className="create-todo-container" onSubmit={handleSubmit}>
-        <input className="create-todo-input" type="text" onChange={handleInputChange} value={input} />
-        <button className="create-todo-button">Add Todo</button>
-      </form>
-    
+    <form className="create-todo-container" onSubmit={handleSubmit}>
+      <input
+        className="create-todo-input"
+        type="text"
+        onChange={handleInputChange}
+        placeholder="Add some quest to your diary... ;)"
+        value={input}
+      />
+      <button className="create-todo-button">Add Todo</button>
+    </form>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import CreateTodo from "./CreateTodo";
-import {FaWindowClose} from 'react-icons/fa';
-import {RiEditBoxFill} from 'react-icons/ri';
-import {VscEdit} from 'react-icons/vsc';
-import {VscError} from 'react-icons/vsc';
+import { FaWindowClose } from "react-icons/fa";
+import { RiEditBoxFill } from "react-icons/ri";
+import { VscEdit } from "react-icons/vsc";
+import { VscError } from "react-icons/vsc";
 
 export const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -14,19 +14,37 @@ export const TodoList = () => {
     console.log([...todos]);
   };
 
+  const handleEdit = (id) => {};
+
+  const handleDelete = (id) => {
+    const updatedTodos = [...todos].filter((todo) => id !== todo.id);
+    setTodos(updatedTodos);
+  };
+
   const todoMapping = [...todos].map((todo) => (
     <div className="todo-item" key={todo.id}>
-     <div> <p>{todo.text}</p></div>
-      <div><span className="icon-edit"><VscEdit/></span>
-      <span className="icon-close"><VscError/></span></div>
+      <div>
+        {" "}
+        <p>{todo.text}</p>
+      </div>
+      <div>
+        <span onClick={() => handleEdit(todo.id)} className="icon-edit">
+          <VscEdit />
+        </span>
+        <span onClick={() => handleDelete(todo.id)} className="icon-close">
+          <VscError />
+        </span>
+      </div>
     </div>
   ));
 
   return (
     <div className="todo-container">
-    
       <CreateTodo addTodo={addTodo} />
-    <h3 className="title">What's the plan fot today?</h3>
+      <div className="suppose-to-be-funny-text-container">
+        <h3 className="title">Time to complete some Quests ;)</h3>
+        <h4 className="subtitle">Isn't life just a RPG game?</h4>
+      </div>
       {todoMapping}
     </div>
   );
